d39058cbe9c2843b25bde236f85072265a842486
Diego López <dlopez@etsfactory.com>
2018-10-01 16:10:18 +0200

    The rabbitmq handler now can receive an argument to listen for specific queue - Deleted unused files

--- rabbitmq_handler.py
+++ rabbitmq_handler.py
@@ -6,20 +6,20 @@ class RabbitMqHandler:
 
     def consume(self, queue):
         self.channel.basic_consume(self.on_message,
-                        queue='hello',
+                        queue=queue,
                         no_ack=True)
 
         print(' [*] Waiting for messages. To exit press CTRL+C')
-        self.channel.start_consuming().getLogger(queue)
+        self.channel.start_consuming()
 
     def init(self):
 
-        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        connection = pika.BlockingConnection(pika.ConnectionParameters(self.url))
         self.channel = connection.channel()
 
         # It ensures that the queue exists
         self.channel.queue_declare(queue='hello')
-        self.consume('test1')
+        self.consume('hello')
 
     # This funcions is executed when a message is received
     def on_message(ch, method, properties, body):
