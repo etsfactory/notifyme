b40fd9da97a5b2932e801d5be4d7427e3a079108
Diego López <dlopez@etsfactory.com>
2018-10-02 15:55:50 +0200

    Code linting and exceptions

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 """
 Main file program.
 """
-from handlers.rabbitmq import RabbitMqHandler
-from handlers.smtp import SMTPHandler
 import json
 import os.path
+
+from notify_me.handlers.rabbitmq import RabbitMqHandler
+from notify_me.handlers.smtp import SMTPHandler
 
 try:
     config_file = 'config.json'
     if os.path.isfile('config_development.json'):
-      config_file = 'config_development.json'
+        config_file = 'config_development.json'
 
     with open(config_file) as json_data:
         d = json.load(json_data)
         email_conf = d["email"]
-        smtp = SMTPHandler(email_conf["username"], email_conf["password"], email_conf["smtp_host"], email_conf["smtp_port"])
-        smtp.send_email('dlopez@ets.es','dlopez@ets.es', 'eeeeeeeeeee desde python')
-except:
+        smtp = SMTPHandler(email_conf["username"], email_conf["password"],
+                           email_conf["smtp_host"], email_conf["smtp_port"])
+        smtp.send_email('dlopez@ets.es', 'dlopez@ets.es', 'eeeeeeeeeee desde python')
+
+except IOError:
     print 'JSON loading problem'
 
 # handler = RabbitMqHandler(server)
