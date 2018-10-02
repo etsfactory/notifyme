4b84f8e92d0ad506672756a7e52eb10e835ca38b
Diego López <dlopez@etsfactory.com>
2018-10-02 16:25:22 +0200

    Merge branch 'feature/send-email' into develop

--- handlers/smtp.py
+++ handlers/smtp.py
@@ -17,12 +17,6 @@ class SMTPHandler(object):
     """
     SMTPHandler class to send emails
     """
-    username = ''
-    password = 'Frost45200'
-    host = ''
-    port = ''
-    server = None
-
     def __init__(self, username, password, host, port):
         """
         Initializes the connection with SMTP server provided and login with username and password
@@ -38,12 +32,13 @@ class SMTPHandler(object):
         except SMTPAuthenticationError:
             print 'Error login with username: ', username, 'and password: ', password
 
-    def send_email(self, sent_from, sent_to, email_text):
+    def send_email(self, sent_to, subject, email_text):
         """
         Send temail trough SMTP from account to a list of emails (sent_to) with a message
         """
         try:
-            self.server.sendmail(sent_from, sent_to, email_text)
+            message = 'Subject: {}\n\n{}'.format(subject, email_text)
+            self.server.sendmail(self.username, sent_to, message)
             self.server.close()
             print 'Email sent to ', sent_to
         except SMTPSendEmailError:
--- main.py
+++ main.py
@@ -4,23 +4,22 @@ Main file program.
 import json
 import os.path
 
-from notify_me.handlers.rabbitmq import RabbitMqHandler
-from notify_me.handlers.smtp import SMTPHandler
+from handlers.rabbitmq import RabbitMqHandler
+from handlers.smtp import SMTPHandler
 
 try:
     config_file = 'config.json'
     if os.path.isfile('config_development.json'):
         config_file = 'config_development.json'
-
-    with open(config_file) as json_data:
-        d = json.load(json_data)
-        email_conf = d["email"]
-        smtp = SMTPHandler(email_conf["username"], email_conf["password"],
-                           email_conf["smtp_host"], email_conf["smtp_port"])
-        smtp.send_email('dlopez@ets.es', 'dlopez@ets.es', 'eeeeeeeeeee desde python')
-
 except IOError:
     print 'JSON loading problem'
+
+with open(config_file) as json_data:
+    d = json.load(json_data)
+    email_conf = d["email"]
+    smtp = SMTPHandler(email_conf["username"], email_conf["password"],
+                       email_conf["smtp_host"], email_conf["smtp_port"])
+    smtp.send_email('dlopez@ets.es', 'Pruebas desde python', 'Otro correo desde python')
 
 # handler = RabbitMqHandler(server)
 # handler.init()
