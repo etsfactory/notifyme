cb603c81b245c156d9cec90a9b6e9aa35374c890
Diego López <dlopez@etsfactory.com>
2018-10-02 13:04:59 +0200

    Development config file is acepted (untracked by git)

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ Main file program.
 from handlers.rabbitmq import RabbitMqHandler
 from handlers.smtp import SMTPHandler
 import json
+import os.path
 
 try:
-    with open('config.json') as json_data:
+   config_file = 'config.json'
+    if os.path.isfile('config_development.json'):
+        config_file = 'config_development.json'
+
+    with open(config_file) as json_data:
         d = json.load(json_data)
         smtp = SMTPHandler(d["email"]["username"],d["email"]["password"])
         smtp.send_email('dlopez@ets.es','dlopez@ets.es', 'eeeeeeeeeee desde python')
