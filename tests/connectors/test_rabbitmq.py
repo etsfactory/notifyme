cda22b9ba72d838a0d877edb7b69ed03a334be67
frostq <diego.lopezgarcia@hotmail.com>
2020-03-27 09:28:15 +0100

    Limpieza de código

--- tests/connectors/test_rabbitmq.py
+++
@@ -1,6 +0,0 @@
-from notify_me.connectors.rabbitmq import RabbitMqHandler
-
-
-def test_rabbitmq_init():
-    rabbit = RabbitMqHandler('localhost', 0, 'hello', 'direct_logs')
-    assert rabbit is not None
